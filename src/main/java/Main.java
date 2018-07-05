d4b7f3211c7dbed756b1a8319704515a31ee8efa
slimakanzer <official.gleb@yandex.ru>
2018-07-05 15:15:23 +0300

    Added some comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,24 @@
-import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class Main {
     public static void main(String[] args){
         Rays ray = new Rays(3, null);
         DatabaseProtocol.getData();
 
         ManagerORM<Rays> managerORM = new ManagerORM<>(Rays.class, DatabaseProtocol.url, DatabaseProtocol.login, DatabaseProtocol.password);
+        //Create table in DB
         managerORM.create();
-        managerORM.update(new Rays(1, new Rays(2, null)));
+
+        //This exaple of recoursive reference
+        managerORM.insert(new Rays(1, new Rays(2, null)));
+
+
         managerORM.insert(ray);
+        //Example of update in ORM
         ray.setName("Name 2");
         managerORM.update(ray);
+
+        //Drop table in DB
         managerORM.dropTable();
 
-        //managerORM.dropTable();
 
     }
 }
